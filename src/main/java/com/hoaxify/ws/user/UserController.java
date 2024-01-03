3a9cfb381ac2e2f9aa29d0ecebe7d6ebef0c174a
altanozturk <altan317@gmail.com>
2024-01-04 01:11:23 +0300

    up

--- src/main/java/com/hoaxify/ws/user/UserController.java
+++ src/main/java/com/hoaxify/ws/user/UserController.java
@@ -1,11 +1,15 @@
 package com.hoaxify.ws.user;
 
+import java.util.HashMap;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.hoaxify.ws.error.ApiError;
 import com.hoaxify.ws.shared.GenericMessage;
 
 @RestController
@@ -17,10 +21,29 @@ public class UserController {
     UserService usersService;
 
     @PostMapping("/api/v1/users")
-    GenericMessage createUser(@RequestBody User user){
-        
+    ResponseEntity<?> createUser(@RequestBody User user) { // ne döneceğini bilmiyorsak <?>
+        ApiError apiError = new ApiError();
+        apiError.setPath("/api/v1/users");
+        apiError.setMessage("Validation error");
+        apiError.setStatus(400);
+        Map<String, String> validationErrors = new HashMap<>();
+        if (user.getUsername() == null || user.getUsername().isEmpty()) {
+            validationErrors.put("username", "Username cannot be null");
+        }
+
+        if (user.getEmail() == null || user.getEmail().isEmpty()) {
+            validationErrors.put("email", "Email cannot be null");
+        }
+
+        if (validationErrors.size() > 0) {
+            apiError.setValidationErrors(validationErrors);
+            return ResponseEntity.badRequest().body(apiError);
+            // badRequest() 400 dönecek
+            // ek olarak apiError nesnesi dönecek
+        }
+
         usersService.save(user);
-        return new GenericMessage("User is created");
+        return ResponseEntity.ok(new GenericMessage("User is created")); // ok başarılı
     };
-    
+
 };
