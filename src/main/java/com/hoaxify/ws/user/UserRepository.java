cc51a8e1b865b6f9e040ec0cf2366d56666ec7ea
altanozturk <altan317@gmail.com>
2024-01-09 23:09:45 +0300

    info

--- src/main/java/com/hoaxify/ws/user/UserRepository.java
+++ src/main/java/com/hoaxify/ws/user/UserRepository.java
@@ -2,6 +2,8 @@ package com.hoaxify.ws.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+// CRUD işlemleri yapan class JpaRepository save, create gibi fonksiyonları mevcut
+
 public interface UserRepository extends JpaRepository<User, Long> {
 
     
